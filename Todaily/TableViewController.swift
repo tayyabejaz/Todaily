966b25fe966de4499f964f517124bac203fdc107
Tayyab Ejaz <tayyabejaz@macbooks-macbook-pro.local>
2018-09-17 19:49:23 +0500

    Check and Uncheck Added

---
+++ Todaily/TableViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  Todaily
+//
+//  Created by Tayyab Ejaz on 17/09/2018.
+//  Copyright © 2018 Tayyab Ejaz. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController{
+    let itemArray = ["Find Mike", "Buy Something", "Attend the Meeting"]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    //MARK - TableView DataSource Methods
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "ToDoItemCell")
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    //MARK - TableView Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //print(itemArray[indexPath.row])
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark
+        {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+
+        }
+        else
+        {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+    }
+}
+
